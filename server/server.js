fe5a2e6ae54de8945b9b61017227b8adc073ac91
rehan-abdullah <31924510+rehan-abdullah@users.noreply.github.com>
2020-12-11 19:01:12 +0000

    Created and tested READ/GET routes for restaurants

--- server/server.js
+++ server/server.js
@@ -3,12 +3,36 @@ const express = require("express");
 
 const app = express()
 
-// Route to get restaurants
-app.get("/get-restaurants", (req, res) => {
-  res.status(418).json({
+// Route to GET ALL restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+  res.status(200).json({
     status: "success",
-    restaurant: "McDonalds"
+    data: {
+      restaurant: ["McDonalds", "Nandos"]
+    }    
   });
+})
+
+// Route to GET a restaurant by ID
+app.get("/api/v1/restaurants/:id", (req, res) => {
+  console.log(req.params);
+  res.status(200).json({
+    status: "success",
+    data: {
+      response: req.params
+    }    
+  });
+})
+
+// Route to GET a restaurant by ID
+app.post("/api/v1/restaurants/", (req, res) => {
+  console.log(req);
+  // res.status(200).json({
+  //   status: "success",
+  //   data: {
+  //     response: req.params
+  //   }    
+  // });
 })
 
 // port defaults to 3001 if PORT not provided by '.env'
